70336a41d7943bc1d1cc451ade641d6cdf2b46b9
William Lago <williamlago2006@gmail.com>
2025-09-04 10:55:49 -0300

    //Adicionando a logica de login

--- Node/Projeto_AGENDA/src/Controllers/loginController.js
+++ Node/Projeto_AGENDA/src/Controllers/loginController.js
@@ -1,7 +1,12 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req,res) =>{
-    res.render('login')
+    if(req.session.user){
+        return res.render('login_logado')
+    }
+    
+    // console.log(req.session.user)
+    return res.render('login')
 }
 
 exports.register = async function(req,res){
@@ -26,4 +31,34 @@ exports.register = async function(req,res){
         return res.render('404')
     }
     
+}
+
+exports.login = async function(req,res){
+    try{
+        const login = new Login(req.body)
+        await login.login()
+
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors) //menssagens
+            req.session.save(function() {
+                return res.redirect(req.get('referer') || '/login/index')
+            })
+            return
+        }
+
+         req.flash('success', 'Usuário Logado com Sucesso') //menssagens
+         req.session.user = login.user   
+         req.session.save(function() {
+                return res.redirect(req.get('referer') || '/login/index')
+            })
+    }catch(e){
+        console.log(e)
+        return res.render('404')
+    }
+    
+}
+
+exports.logout = function(req, res){
+    req.session.destroy()
+    res.redirect('/')
 }
