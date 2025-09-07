29fab0773232e6eba0dab2fb151412fd74f460eb
William Lago <williamlago2006@gmail.com>
2025-09-07 11:39:02 -0300

    //Inserindo o contato

--- Node/Projeto_AGENDA/routes.js
+++ Node/Projeto_AGENDA/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const route = express.Router()
+
 const homeController = require('./src/Controllers/homeController')
 const loginController = require('./src/Controllers/loginController')
+const contatoController = require('./src/Controllers/contatoController')
+
+const { loginRequired }  = require('./src/Middlewares/middleware')
 
 //Rotas da Home
 route.get('/',  homeController.index) 
@@ -11,5 +15,10 @@ route.get('/login/index',loginController.index)
 route.post('/login/register',loginController.register)
 route.post('/login/login',loginController.login)
 route.get('/login/logout',loginController.logout)
+
+//Rotas de Contato
+route.get('/contato/index', loginRequired, contatoController.index)
+route.post('/contato/register', loginRequired, contatoController.register)
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex)
 
 module.exports = route;
